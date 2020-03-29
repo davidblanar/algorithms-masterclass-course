1d9a0ad4b7ab469fa39a7b9161244075f501adb9
David Blanar <david.blanar@gmail.com>
2020-03-29 15:22:31 +0200

    more sorting

--- script.js
+++ script.js
@@ -329,6 +329,7 @@ function swap(a, i1, i2) {
   a[i1] = a[i2];
   a[i2] = temp;
 }
+
 function bubbleSort(arr) {
   if (arr.length < 2) {
     return arr;
@@ -433,7 +434,71 @@ function mergeSort(arr) {
   }
 
   const mid = Math.floor(arr.length / 2);
-  return merge(mergeSort(arr.slice(0, mid)), mergeSort(arr.slice(mid)));
+  const leftArr = mergeSort(arr.slice(0, mid));
+  const rightArr = mergeSort(arr.slice(mid));
+  return merge(leftArr, rightArr);
+}
+
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  function pivot(a) {
+    let pivotIdx = 0;
+    const pivot = a[pivotIdx];
+    for (let i = 1; i < a.length; i++) {
+      if (pivot > a[i]) {
+        pivotIdx++;
+        swap(a, i, pivotIdx);
+      }
+    }
+    swap(a, 0, pivotIdx);
+    return pivotIdx;
+  }
+  const pivotIdx = pivot(arr);
+  const leftArr = arr.slice(0, pivotIdx + 1);
+  const rightArr = arr.slice(pivotIdx + 1);
+  return quickSort(leftArr).concat(quickSort(rightArr));
+}
+
+function radixSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  function getDigit(int, idx) {
+    return Math.floor(Math.abs(int) / Math.pow(10, idx)) % 10;
+  }
+
+  function digitCount(int) {
+    if (int === 0) {
+      return 1;
+    }
+    return Math.floor(Math.log10(Math.abs(int))) + 1;
+  }
+
+  function mostDigits(a) {
+    let highest = 0;
+    for (let i = 0; i < a.length; i++) {
+      const count = digitCount(a[i]);
+      if (count > highest) {
+        highest = count;
+      }
+    }
+    return highest;
+  }
+
+  const largestNumDigitsCount = mostDigits(arr);
+  for (let i = 0; i < largestNumDigitsCount; i++) {
+    const buckets = Array.from(Array(10)).map(() => []);
+    for (let j = 0; j < arr.length; j++) {
+      const digit = getDigit(arr[j], i);
+      buckets[digit].push(arr[j]);
+    }
+    arr = [].concat(...buckets);
+  }
+  return arr;
 }
 
 module.exports = {
@@ -459,5 +524,7 @@ module.exports = {
   bubbleSort,
   selectionSort,
   insertionSort,
-  mergeSort
+  mergeSort,
+  quickSort,
+  radixSort
 };
--- script.test.js
+++ script.test.js
@@ -21,7 +21,9 @@ const {
   bubbleSort,
   selectionSort,
   insertionSort,
-  mergeSort
+  mergeSort,
+  quickSort,
+  radixSort
 } = require("./script");
 
 describe("frequency counter pattern", () => {
@@ -232,5 +234,31 @@ describe("sorting", () => {
     expect(mergeSort([2,3,1])).toEqual([1,2,3]);
     expect(mergeSort([5,3,4,1,2,6,9,8,7])).toEqual([1,2,3,4,5,6,7,8,9]);
     expect(mergeSort([8,1,2,3,4,5])).toEqual([1,2,3,4,5,8]);
+  });
+
+  it("quickSort", () => {
+    expect(quickSort([])).toEqual([]);
+    expect(quickSort([1])).toEqual([1]);
+    expect(quickSort([1,2])).toEqual([1,2]);
+    expect(quickSort([2,1])).toEqual([1,2]);
+    expect(quickSort([2,3,1])).toEqual([1,2,3]);
+    expect(quickSort([8,1,2])).toEqual([1,2,8]);
+    expect(quickSort([5,3,4,1,2,6,9,8,7])).toEqual([1,2,3,4,5,6,7,8,9]);
+    expect(quickSort([8,1,2,3,4,5])).toEqual([1,2,3,4,5,8]);
+  });
+
+  it("radixSort", () => {
+    expect(radixSort([])).toEqual([]);
+    expect(radixSort([1])).toEqual([1]);
+    expect(radixSort([1,2])).toEqual([1,2]);
+    expect(radixSort([11,2])).toEqual([2,11]);
+    expect(radixSort([2,1])).toEqual([1,2]);
+    expect(radixSort([2,3,1])).toEqual([1,2,3]);
+    expect(radixSort([8,1,2])).toEqual([1,2,8]);
+    expect(radixSort([5,3,4,1,2,6,9,8,7])).toEqual([1,2,3,4,5,6,7,8,9]);
+    expect(radixSort([8,1,2,3,4,5])).toEqual([1,2,3,4,5,8]);
+    expect(radixSort([80,144,2])).toEqual([2,80,144]);
+    expect(radixSort([1556,4,3556,593,408,4386,902,7,8157,86,9637,29]))
+      .toEqual([4,7,29,86,408,593,902,1556,3556,4386,8157,9637]);
   });
 });
